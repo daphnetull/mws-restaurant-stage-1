66699bbb7a2d8bbbeb0f0211f7e528913790ab02
daphnetull <palebluebeyond@icloud.com>
2018-09-17 14:54:05 -0400

    Add files via upload

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `https://daphnetull.github.io/mws-restaurant-stage-1/data/restaurants.json`;
+    return `http://localhost:${port}/data/restaurants.json`;
   }
 
   /**
