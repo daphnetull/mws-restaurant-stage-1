a2e985d23398847c187a4bc55f819942eeded16b
daphnetull <palebluebeyond@icloud.com>
2018-09-17 14:22:39 -0400

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `https://github.com/daphnetull/mws-restaurant-stage-1/data/restaurants.json`;
+    return `https://daphnetull.github.io/mws-restaurant-stage-1/data/restaurants.json`;
   }
 
   /**
