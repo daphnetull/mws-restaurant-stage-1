cbedacb3fb43a6f1ceab0d8e15574db68f43d799
daphnetull <palebluebeyond@icloud.com>
2018-09-18 13:43:54 -0400

    added fetch event listener to the service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
+/**
+ *  Caching all of the pages using a listener for the install event.  
+**/
+
 self.addEventListener('install', function(event){
-  const cachedUrls = [
+  const cachedUrls = [  // storing all of the page and resource file names into an array
     '/',
     '/index.html',
     '/restaurant.html',
@@ -18,14 +22,31 @@ self.addEventListener('install', function(event){
     '/js/main.js',
     '/js/dbhelper.js',
     '/js/restaurant_info.js'
-	];
+  ];
 
-  event.waitUntil(
-  	caches.open('restaurant-reviews-v1').then(function(cache){
-  		console.log('cached!');
-  		return cache.addAll(cachedUrls);
-
-  	})
+  event.waitUntil( // once the installation is done, a promise is used to cache the URLs into the restaurant-reviews-v1 cache name
+    caches.open('restaurant-reviews-v1').then(function(cache){
+      return cache.addAll(cachedUrls);
+      console.log('cached!');
+    })
 
   )
-}) 
+})
+
+/**
+ *  fetch function is used to load the cached pages in offline mode
+**/
+
+self.addEventListener('fetch', function(event){
+  // interrupting default fetch event and responding to the fetch event object 
+  event.respondWith(
+    // using a promise to determine if resource is already cached 
+    // by comparing what's being requested to what's already cached
+    caches.match(event.request).then(function(response){    
+      // if resource already exists in cache, simply return it
+      if (response) return response;
+      // otherwise, continue default action of requesting resource from the network 
+      return fetch(event.request);
+    })
+  );
+});
